2dc584dc8ac298973c638c520b8cae6768d48bac
Jriquer <jazzriquer@gmail.com>
2025-03-20 17:54:14 -0400

    Create script2.js

---
+++ 01-variables-data-types/10-capitalize-challenge/script2.js
@@ -0,0 +1,10 @@
+// capitilize challenge personal solution 
+
+
+const myString = 'developer';
+
+let myNewString
+
+myNewString = myString[0].toUpperCase() + myString.substring(1);
+
+console.log(myNewString); // Developer 
